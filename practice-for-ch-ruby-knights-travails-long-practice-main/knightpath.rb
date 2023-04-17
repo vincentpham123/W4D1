8bd2f44a3b8d69cd1de7c7e8bd6d29304e3307e9
vincentpham123 <vincentpham32@gmail.com>
2023-04-17 14:37:22 -0700

    finished phase 2

--- practice-for-ch-ruby-knights-travails-long-practice-main/knightpath.rb
+++ practice-for-ch-ruby-knights-travails-long-practice-main/knightpath.rb
@@ -1,8 +1,10 @@
 require_relative 'polytree'
 class KnightPathFinder
     attr_reader :considered_positions, :initial_position
+    attr_accessor :root_node
+
     def initialize (initial_position)
-    @initial_position = PolyTreeNode.new(initial_position)
+    @initial_position = initial_position
     @considered_positions=[initial_position]
     end
 
@@ -19,31 +21,35 @@ class KnightPathFinder
     end
 
     def new_move_positions(pos)
+        result = []
         valid_moves = KnightPathFinder.valid_moves(pos)
         valid_moves.each do |pos|
-            @considered_positions<< pos if !@considered_positions.include?(pos)
+            if !@considered_positions.include?(pos)
+                @considered_positions<< pos
+                result << pos
+            end
         end
+        result
     end
      #Phase 2
 
-    def build_move_tree(target)
-        
-
-        until @considered_positions.empty? 
-            check = queue.shift 
-            if check==target 
-                return target
-            else
-                new_move_positions(check)
+    def build_move_tree
+        self.root_node = PolyTreeNode.new(initial_position)
+        queue=[root_node]
+        until queue.empty?
+            check = queue.shift
+            check_pos = check.value
+            moves = new_move_positions(check_pos)
+            moves.each do |next_move|
+                next_node = PolyTreeNode.new(next_move)
+                queue << next_node
+                check.add_child(next_node)
             end
         end
-        nil
     end
-
+end
 
 
         
 
 
-    
-end
